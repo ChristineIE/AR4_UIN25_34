90c13d765bc5713e46269837476a902000c0c947
krismoe9 <krismoe@hiof.no>
2026-04-26 16:00:52 +0200

    Update App.jsx
    Added <Elstad /> and <Moe />

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import AssignmentCard from "./components/AssignmentCard";
+import Elstad from "./components/Elstad"
+import Moe from "./components/Moe"
 
 function App() {
   return(
 
   <section>
     <Header />
+    <Elstad />
+    <Moe />
     <AssignmentCard />
   </section>
 )}
