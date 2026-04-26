89af443af1cd4cb1fcb206c50bc658cfd77ddf74
krismoe9 <krismoe@hiof.no>
2026-04-26 14:44:53 +0200

    Update Assignment.jsx
    Done with code

--- frontend/src/components/AssignmentCard.jsx
+++ frontend/src/components/AssignmentCard.jsx
@@ -1,22 +1,29 @@
 import { useState, useEffect } from "react";
-import { createClient } from "@sanity/client";
+import client from "../helpers/client.js";
 
 export default function AssignmentCard() {
   const [assignment, setAssignment] = useState([]);
-  const [loading, setLoading] = useState(true);
+  //const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchAssignments() {
-      const query = await client.fetch(
-        "*[_type == 'assignments']{title, content}",
-      );
+      const query = await client.fetch("*[_type == 'assignments']{_id, title, content}")
       setAssignment(query);
     }
 
-    fetchAssignments();
-  }, []);
+    fetchAssignments()
+  }, [])
 
-  console.log(assignment);
+  console.log(assignment)
 
-  return <article>{assignment?.map()}</article>;
+  return (
+    <section>
+      {assignments?.map((a) => (
+        <article key={a._id}>
+          <h2>{a.title}</h2>
+          <p>{a.content}</p>
+        </article>
+      ))}
+    </section>
+  )
 }
